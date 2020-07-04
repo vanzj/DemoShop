89886023e38319e45be5bc816cc86a4398a24c85
vanzj <1508347118@qq.com>
2020-07-04 12:03:02 +0800

    尝试构建

--- DemoShop/Controllers/WeatherForecastController.cs
+++ DemoShop/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DemoShop.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
